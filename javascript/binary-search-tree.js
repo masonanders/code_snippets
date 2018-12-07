1da1deb42214ac705333834f608b2a50fc30a24e
MasonA <mason@ourprairie.net>
2018-12-07 11:43:56 -0800

    add bst functions

--- javascript/binary-search-tree.js
+++ javascript/binary-search-tree.js
@@ -4,3 +4,9 @@ function BinarySearchTree(root) {
   if (root !== "object") root = new BinaryNode(root);
   this.root = root;
 }
+
+BinarySearchTree.prototype.insert = function (value) {
+};
+
+BinarySearchTree.prototype.findInsertNode = function (value) {
+};
